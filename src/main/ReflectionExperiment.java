b02b73deed4caf53a7e55bd079001d5ed1cf6462
Sanjshine99 <sanubandara12639@gmail.com>
2023-12-29 20:49:34 +0530

    feat(): Experiment 11: Access private field

--- src/main/ReflectionExperiment.java
+++ src/main/ReflectionExperiment.java
@@ -51,6 +51,12 @@ public class ReflectionExperiment {
             System.out.println(
                     "Specific Declared Method with Parameters of DoubleClass: " + methodWithParams.toString());
 
+            Field privateField = d.getDeclaredField("b");
+            privateField.setAccessible(true);
+            DoubleClass DoubleClass = new DoubleClass();
+            double fieldValue = (double) privateField.get(DoubleClass);
+            System.out.println("Access Private Field of DoubleClass: " + fieldValue);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
