6b89d5c1fee73d416c03f8e853dc3b12ad3cce26
Sanjshine99 <sanubandara12639@gmail.com>
2023-12-29 20:36:23 +0530

    feat(): Experiment 6: Get declared fields

--- src/main/ReflectionExperiment.java
+++ src/main/ReflectionExperiment.java
@@ -25,6 +25,13 @@ public class ReflectionExperiment {
                     System.out.println("Field " + field.getName() + " is not of primitive type: " + field.getType());
                 }
             }
+
+            Field[] fields2 = d.getDeclaredFields();
+            System.out.println("Declared Fields of DoubleClass: ");
+            for (Field field : fields2) {
+                System.out.println(field.toString());
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
