b0052ab235f4b762607537962ccf4a5a2cc4388f
Sanjshine99 <sanubandara12639@gmail.com>
2023-12-29 16:09:38 +0530

    feat(): Experiment 1: Get class name

--- src/main/ReflectionExperiment.java
+++ src/main/ReflectionExperiment.java
@@ -4,9 +4,10 @@ public class ReflectionExperiment {
 
     public static void main(String[] args) {
         try {
-
+            Class<?> c = DoubleClass.class;
+            System.out.println("Experiment 1 - Class Name: " + c.getName());
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 }
