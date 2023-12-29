27627801d5762fb543e60e9842a379b5cd7a5569
Sanjshine99 <sanubandara12639@gmail.com>
2023-12-29 16:11:21 +0530

    feat(): Experiment 2: Load class using forName

--- src/main/ReflectionExperiment.java
+++ src/main/ReflectionExperiment.java
@@ -6,6 +6,9 @@ public class ReflectionExperiment {
         try {
             Class<?> c = DoubleClass.class;
             System.out.println("Class Name is " + c.getName());
+
+            Class<?> c2 = Class.forName("DoubleClass");
+            System.out.println("Class Loaded with forName " + c2.getName());
         } catch (Exception e) {
             e.printStackTrace();
         }
